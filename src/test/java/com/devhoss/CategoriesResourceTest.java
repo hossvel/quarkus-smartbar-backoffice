ba2d2b3c028e885cd85b1adc5f74e688bc0f08f0
devhoss <velasco2288@gmail.com>
2025-10-31 00:43:27 -0500

    mockito

--- src/test/java/com/devhoss/CategoriesResourceTest.java
+++ src/test/java/com/devhoss/CategoriesResourceTest.java
@@ -9,25 +9,22 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-
 
 @QuarkusTest
 class CategoriesResourceTest {
 
-   // @InjectMock
-   // CategoriesService categoriesServiceMock;
-
+   @InjectMock
+   CategoriesService categoriesServiceMock;
 
     @Test
     void getsListCategories() {
 
-        //when(categoryService.getAll()).thenReturn(List.of(new Category("wine")));
-       // when(categoriesServiceMock.get()).thenReturn(new Category().name("drinks"));
+        Mockito.when(categoriesServiceMock.getAll()).thenReturn(List.of(new Category().name("Mock")));
 
        final Response response = given()
                 .when().get("/categories")
@@ -35,7 +32,7 @@ class CategoriesResourceTest {
                .statusCode(200)
                 .extract().response();
        final JsonPath jsonPath = response.jsonPath();
-       Assertions.assertEquals("drinks",jsonPath.getString("[0].name"));
+       Assertions.assertEquals("Mock",jsonPath.getString("[0].name"));
     }
 
 
