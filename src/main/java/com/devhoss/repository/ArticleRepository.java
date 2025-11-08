d3b5c1698d0241c33fe2f454e8dc25fd1516ca16
devhoss <velasco2288@gmail.com>
2025-11-08 13:35:59 -0500

    group by

--- src/main/java/com/devhoss/repository/ArticleRepository.java
+++ src/main/java/com/devhoss/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.devhoss.repository;
 import com.devhoss.model.Article;
 import com.devhoss.model.Category;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Sort;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 public class ArticleRepository  implements PanacheRepository<Article> {
 
     public List<Article> listByCategory(Category category) {
-        return list("category",  category);
+        return list("category", Sort.by("price", Sort.Direction.Descending), category);
     }
 
 }
